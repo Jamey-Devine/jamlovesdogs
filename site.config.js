e586a778a43681b9a9f9a11052b692df83c419df
samuel-dean-machine <sam.pauldean@gmail.com>
2024-03-03 11:50:09 -0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "jamey devine",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "",
+    bio: "",
+    email: "jameydevine10@gmail.com",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "jamlovesdogs",
+    description: "welcome to jamlovesdogs!",
   },
 
   // CONFIG configration (required)
